6c99570d421c46899aaf6c3d689f6a0de4f1efe4
luisvalesilva <luisvalesilva@gmail.com>
2016-10-23 23:56:51 -0400

    First commit

---
+++ plot_signal_at_orf.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+"""Plot the output of signal_at_orf.py, after grouping by position and calculating the mean"""
+
+import pandas as pd
+import pylab as pl
+
+def main():
+    print()
+    print("---------------------------------------------------------")
+    print("Computing average signal by position for data in file(s):")
+    print(args.input_data_a)
+    if args.input_data_b:
+        print(args.input_data_b)
+    print()
+
+    data_a = pd.read_csv(args.input_data_a, sep='\t')
+    grouped_a = data_a.groupby(['position'], as_index=False).mean()
+    xa = grouped_a['position']
+    ya = grouped_a['signal']
+
+    if args.input_data_b:
+        data_b = pd.read_csv(args.input_data_b, sep='\t')
+        grouped_b = data_b.groupby(['position'], as_index=False).mean()
+        xb = grouped_b['position']
+        yb = grouped_b['signal']
+
+    print('Plotting...')
+    print("---------------------------------------------------------")
+
+    pl.clf()
+    pl.plot(xa, ya, label=args.input_data_a)
+    if args.input_data_b:
+        pl.plot(xb, yb, label=args.input_data_b)
+    pl.legend()
+    pl.show()
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description="Plot output of signal_at_orf.py")
+    parser.add_argument('-a', '--input_data_a', help=("path to a TSV file"), required=True)
+    parser.add_argument('-b', '--input_data_b', help=("path to a TSV file"))
+    args = parser.parse_args()
+    main()
